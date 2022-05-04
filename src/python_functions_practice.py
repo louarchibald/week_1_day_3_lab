3435796feababa18681326cf7934bb73fb18bfb5
Codeclan_Student <codeclan_student@Codeclan-Students-MacBook-Pro.local>
2022-05-04 15:44:25 +0100

    third commit

--- src/python_functions_practice.py
+++ src/python_functions_practice.py
@@ -6,3 +6,22 @@ def add(num1, num2):
 
 def subtract(num1, num2):
     return num1 - num2
+
+def divide(num1, num2):
+    return num1 / 2
+
+def join_string(string_1, string_2):
+    return string_1 + string_2
+
+months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+def number_to_full_month_name(month1):
+    return months [month1 - 1]
+
+def number_to_short_month_name(month1):
+    month = months[month1 - 1]
+    return month[:3]
+
+def number_to_short_month_name(month10):
+    month = months[month10 - 1]
+    return month[:3]
